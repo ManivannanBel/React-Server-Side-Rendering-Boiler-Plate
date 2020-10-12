b6b1e32666bf3194b7bec1c5a088a29a69751030
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-10-12 07:16:37 +0530

    react server side boiler plate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+const app = express();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+app.get('/', (req, res) => {
+    const content = renderToString(<Home/>);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    res.send(content);
+});
+
+app.listen(3000, () => {
+    console.log('Listening to localhost:3000')
+});
